27d35467ba53fc51657824e01be268376614561f
George Pavlides <p-george18@hotmail.com>
2021-03-28 16:01:16 +0300

    Minor changes in UserData index page headings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+has_one :user_datum, dependent: :destroy
 end
--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -1,2 +1,3 @@
 class UserDatum < ApplicationRecord
+	belongs_to :user
 end
