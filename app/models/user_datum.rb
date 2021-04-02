2ed0de75d51f781af88c552140a6f10ee94fceba
George Pavlides <p-george18@hotmail.com>
2021-04-02 17:40:27 +0300

    Added calculation of BMI and user_id field for each user datum creation

--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -3,10 +3,8 @@ class UserDatum < ApplicationRecord
 	belongs_to :user
 
 	scope :user_user_data, ->(user){where(['user_id = ?',user.id])}
-
+	
 	def calculate_BMI
       self.BMI=(self.weight/((self.height.to_f/100)**2)).round(1)
-
     end
-
 end
