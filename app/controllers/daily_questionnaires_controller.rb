de4d936a79f45cb694072c322725ebe595b277f2
Brian Liang <brianbrain2010@hotmail.com>
2021-04-25 18:17:56 +0100

    Added_different_questionnaire_views_and_popup_modal

--- app/controllers/daily_questionnaires_controller.rb
+++ app/controllers/daily_questionnaires_controller.rb
@@ -1,7 +1,12 @@
 class DailyQuestionnairesController < ApplicationController
   before_action :set_daily_questionnaire, only: %i[ show edit update destroy ]
+  before_action :set_today_time
   require 'date'
   
+  def set_today_time
+    @dayOfWeek=Date.today.strftime('%A')
+  end
+
   # GET /daily_questionnaires or /daily_questionnaires.json
   def index
     @daily_questionnaires = DailyQuestionnaire.all
@@ -21,18 +26,30 @@ class DailyQuestionnairesController < ApplicationController
   end
 
   # POST /daily_questionnaires or /daily_questionnaires.json
+  # Creating and saving new daily questionnaire should cause a new instance of user_daily_questionnaire to be created
+  # and every fields to be set up correctly for information of that day
   def create
+    # Initializing current user data as the current user
     @currentUserData = UserDatum.user_user_data(current_user)
+    # A new daily questionnaire to be created and its day of week field being set to the time using date
     @daily_questionnaire = DailyQuestionnaire.new(daily_questionnaire_params)
-    @daily_questionnaire.dayOfWeek=Date.today.strftime('%A')
+    @daily_questionnaire.dayOfWeek=@dayOfWeek
+
     respond_to do |format|
       if @daily_questionnaire.save
+        # If a daily questionnaire is created and saved
+        # Create a new user_daily_questionnaire instance and set all its fields to be associated with user
+        @new_User_Daily_Questionnaire = UserDailyQuestionnaire.new(questionnaireDate: Date.today, user_data: @currentUserData, user_datum_id: @currentUserData.ids.first)
+        # Save the created user_daily_questionnaire
+        @new_User_Daily_Questionnaire.save
+        # Set the association of the created user_daily_questionnaire and daily_questionnaire
+        @daily_questionnaire.user_daily_questionnaires=[@new_User_Daily_Questionnaire]
+        @daily_questionnaire.user_daily_questionnaire_id=@new_User_Daily_Questionnaire.id
+        @daily_questionnaire.save
+        # Redirect back to root path after new daily questionnaire is created
         format.html { redirect_to root_path, notice: "Daily questionnaire was successfully created." }
         format.json { render :show, status: :created, location: @daily_questionnaire }
-        new_User_Daily_Questionnaire = UserDailyQuestionnaire.new(questionnaireDate: Date.today, user_data: @currentUserData, user_datum_id: @currentUserData.ids.first)
-        new_User_Daily_Questionnaire.save!
-        @daily_questionnaire.user_daily_questionnaires=[new_User_Daily_Questionnaire]
-        @daily_questionnaire.user_daily_questionnaire_id=new_User_Daily_Questionnaire.id
+
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @daily_questionnaire.errors, status: :unprocessable_entity }
--- app/models/daily_questionnaire.rb
+++ app/models/daily_questionnaire.rb
@@ -1,7 +1,7 @@
 class DailyQuestionnaire < ApplicationRecord
-    # Daily Questionnaires can have many versions, eg: A version for each day of a week.
-    # So the questionnaire for this day of a week can be used by "user_daily_questionnaires" objects on the corresponding dates
-    has_many :user_daily_questionnaires
+    # A daily questionnaire has one user daily questionnaire as record, but association was built as has_many
+    # May change afterwards
+    has_many :user_daily_questionnaires, dependent: :destroy
 
     # Validate all parameters to be presence
     validates :user_mood, :duration_mins,:indoor_score,:outdoor_score,:cardio_score,:strength_score,:physicality_score,:mentality_score,:solo_score, :solo_score, :team_score,:intensity_score, presence: true
@@ -9,5 +9,5 @@ class DailyQuestionnaire < ApplicationRecord
     
     daysOfWeek=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     validates :dayOfWeek, presence: true, inclusion: { in: daysOfWeek, message: "%{value} is not a valid day of the week, please re-enter a day of the week" } 
-    
+
 end
