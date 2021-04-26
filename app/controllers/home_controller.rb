5d52558f42164f99ea9739e80a3791ff5993c019
Brian Liang <brianbrain2010@hotmail.com>
2021-04-26 15:36:07 +0100

    Added_if_statements_for_modal_popups

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+  before_action :set_today_time
+  require 'date'
+  require 'time'
+  
+  # Function to set the day of week as today's week day
+  def set_today_time
+    @dayOfWeek=Date.today.strftime('%A')
+    @currentTime=Time.zone.now.strftime("%H:%M:%S")
+  end
+
   def index
+    # Variables for showing the pop-ups if they don't exist
+    @currentUserData = UserDatum.user_user_data(current_user)
+    @userDailyQuestionnaireToday = UserDailyQuestionnaire.where(user_datum_id: @currentUserData.ids.first, questionnaireDate:Date.today)
+
+    @userFeedbackQuestionnaireToday = UserFeedbackQuestionnaire.where(user_datum_id: @currentUserData.ids.first, questionnaireDate:Date.today)
   end
 
   def about
