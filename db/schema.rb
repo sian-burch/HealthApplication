8c7c2f9a790a91183589d23a40435bb802d3ead6
George Pavlides <p-george18@hotmail.com>
2021-05-05 13:20:27 +0300

    Added weather_restricted field for activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_133204) do
+ActiveRecord::Schema.define(version: 2021_05_05_100946) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_133204) do
     t.integer "intensity_score", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "weather_restricted"
   end
 
   create_table "recommended_daily_steps", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Activity.create(name: 'Running',duration_mins: 30,indoor_score: 2, outdoor_score: 4,cardio_score: 5, strength_score: 0, physicality_score: 5,mentality_score: 2,solo_score: 5,team_score: 4,intensity_score: 4)
-Activity.create(name: 'Walking',duration_mins: 30,indoor_score: 2, outdoor_score: 4,cardio_score: 3, strength_score: 0, physicality_score: 5,mentality_score: 1,solo_score: 5,team_score: 4,intensity_score: 1)
-Activity.create(name: 'Meditation',duration_mins: 15,indoor_score: 5, outdoor_score: 5,cardio_score: 0, strength_score: 0, physicality_score: 0,mentality_score: 5,solo_score: 5,team_score: 3,intensity_score: 0)
+Activity.create(name: 'Running',duration_mins: 30,indoor_score: 2, outdoor_score: 4,cardio_score: 5, strength_score: 0, physicality_score: 5,mentality_score: 2,solo_score: 5,team_score: 4,intensity_score: 4,weather_restricted: true)
+Activity.create(name: 'Walking',duration_mins: 30,indoor_score: 2, outdoor_score: 4,cardio_score: 3, strength_score: 0, physicality_score: 5,mentality_score: 1,solo_score: 5,team_score: 4,intensity_score: 1,weather_restricted: true)
+Activity.create(name: 'Meditation',duration_mins: 15,indoor_score: 5, outdoor_score: 5,cardio_score: 0, strength_score: 0, physicality_score: 0,mentality_score: 5,solo_score: 5,team_score: 3,intensity_score: 0,weather_restricted: false)
 
 #seeds for daily steps recomendations
 for age in 4..6
