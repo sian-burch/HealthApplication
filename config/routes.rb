8c378c3300bcb7689d3b7843bbc223ef430c0eb4
sumeetgurung <73563479+sumeetgurung@users.noreply.github.com>
2021-05-16 18:40:24 +0100

    Update Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :weight_diaries
-  mount RailsAdmin: :Engine => '/admin', as: 'rails_admin'
+  resources :mental_posts
+  resources :posts
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :user_daily_questionnaires
   resources :recommended_daily_steps
   resources :activities
