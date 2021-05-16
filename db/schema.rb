bf441a421307a4f1a3dc66ae818fdb2b47569260
Brian Liang <brianbrain2010@hotmail.com>
2021-05-16 19:49:59 +0100

    New commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_05_16_111112) do
+ActiveRecord::Schema.define(version: 2021_05_16_165547) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,9 +32,6 @@ ActiveRecord::Schema.define(version: 2021_05_16_111112) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-=======
-ActiveRecord::Schema.define(version: 2021_05_16_165547) do
->>>>>>> 887547052732db8b90a3be1e3e6aa403969b79e1
 
   create_table "activities", force: :cascade do |t|
     t.string "name", null: false
