08cdd6cb63bff321af85520c96848dd21170c618
sumeetgurung <73563479+sumeetgurung@users.noreply.github.com>
2021-05-16 18:41:12 +0100

    Update Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_021025) do
+ActiveRecord::Schema.define(version: 2021_05_16_165547) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name", null: false
