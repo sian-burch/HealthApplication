145bd143c759e590aebee6be09ff8b45f099c13a
sumeetgurung <73563479+sumeetgurung@users.noreply.github.com>
2021-05-16 18:48:28 +0100

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,5 +20,7 @@
 // to get the new features in 2.3.0:
 //= require highcharts/highcharts-more
 //= require highcharts/themes/dark-blue
+//= require chartkick
+//= require Chart.bundle
 //= require select2
 //= require cookies_eu
