8eb4030aec9e8f02688fd4da0190d6570c7d86c2
Brian Liang <brianbrain2010@hotmail.com>
2021-05-16 17:46:44 +0100

    Added user profile picture and active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Change this part to any options in config/storage.yml when development
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
